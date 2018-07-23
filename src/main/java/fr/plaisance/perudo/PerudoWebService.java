34594ec7b37050720a45fa340072c4873c9957e4
Romain Warnan <romain.warnan@gmail.com>
2018-07-23 21:32:49 +0200

    Début du passage à Spring MVC à la place de Jersey

--- src/main/java/fr/plaisance/perudo/PerudoWebService.java
+++ src/main/java/fr/plaisance/perudo/PerudoWebService.java
@@ -2,6 +2,16 @@ package fr.plaisance.perudo;
 
 import fr.plaisance.domaine.*;
 import fr.plaisance.exception.PerudoException;
+import fr.plaisance.perudo.domaine.Game;
+import fr.plaisance.perudo.domaine.Player;
+import fr.plaisance.perudo.exception.PerudoException;
+import fr.plaisance.perudo.service.GameService;
+import fr.plaisance.perudo.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -15,22 +25,24 @@ import java.io.IOException;
 
 import static fr.plaisance.PerudoService.*;
 
-@Path("/game")
+@RestController("/game")
 public class PerudoWebService {
-	
-	public static final String ERROR = "error", SUCCESS = "success";
-	
-	@GET
-	@Produces(MediaType.TEXT_XML)
-	@Path("/perudo")
-	public Document perudo(@QueryParam("game") Long gameId, @QueryParam("player") Long playerId) throws SAXException, ParserConfigurationException, IOException {
+
+    @Autowired
+    private GameService gameService;
+
+    @Autowired
+    private PlayerService playerService;
+
+	@GetMapping(value = "/perudo", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Document perudo(@RequestParam("game") Long gameId, @RequestParam("player") Long playerId)  {
 		// System.out.println("perudo : game = " + gameId + ", player = " + playerId);
 		Document document = null;
 		try {
-			Game game = gameService().getById(gameId);
-			Player player = playerService().getById(playerId);
-			playerService().check(player, game);
-			if(gameService().isPaused(game)){
+			Game game = gameService.getById(gameId);
+			Player player = playerService.getById(playerId);
+			playerService.check(player, game);
+			if(gameService.isPaused(game)){
 				document = xmlService().buildFullDocument(game, player);
 			}
 			else{
@@ -48,10 +60,10 @@ public class PerudoWebService {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String addPlayer(@QueryParam("name") String playerName) {
 		System.out.print("addPlayer : name = " + playerName);
-		Game game = gameService().lastGame();
+		Game game = gameService.lastGame();
 		String response = null;
 		try {
-			Player player = playerService().createPlayer(playerName, game);
+			Player player = playerService.createPlayer(playerName, game);
 			game.addMessage(PerudoMessageType.INFO, "Le joueur " + playerName + " a été ajouté à la partie.");
 			response =  SUCCESS + ":" + player.getPlayerId() + ":" + game.getGameId();	
 		}
@@ -70,21 +82,21 @@ public class PerudoWebService {
 		System.out.print("rollDice : player = " + playerId);
 		String response = null;
 		try {
-			Player player = playerService().getById(playerId);
-			Game game = gameService().getById(gameId);
+			Player player = playerService.getById(playerId);
+			Game game = gameService.getById(gameId);
 			PerudoResult result = game.getResult();
 			switch (result.getType()) {
 				case WIN:
-					playerService().winDie(game, result.getPlayer());
+					playerService.winDie(game, result.getPlayer());
 					game.addMessage(PerudoMessageType.INFO, player.getPlayerName() + " a gagné un dé.");
 					break;
 				case LOSE:
-					playerService().loseDie(game, result.getPlayer());
+					playerService.loseDie(game, result.getPlayer());
 					game.addMessage(PerudoMessageType.INFO, player.getPlayerName() + " a perdu un dé.");
 					break;
 			}
-			gameService().rollDice(game);
-			gameService().clearActions(game);
+			gameService.rollDice(game);
+			gameService.clearActions(game);
 			response =  SUCCESS + ":" + player.getPlayerId() + ":" + game.getGameId();	
 		}
 		catch (PerudoException e) {
@@ -101,9 +113,9 @@ public class PerudoWebService {
 		System.out.print("startGame : game = " + gameId);
 		String response = null;
 		try {
-			Game game = gameService().getById(gameId);
-			gameService().startGame(game);
-			gameService().rollDice(game);
+			Game game = gameService.getById(gameId);
+			gameService.startGame(game);
+			gameService.rollDice(game);
 			game.addMessage(PerudoMessageType.INFO, "La partie a débutée.");
 			response = SUCCESS + ":" + gameId;
 		}
@@ -121,8 +133,8 @@ public class PerudoWebService {
 		System.out.print("endGame : game = " + gameId);
 		String response = null;
 		try {
-			Game game = gameService().getById(gameId);
-			Player winner = gameService().winner(game);
+			Game game = gameService.getById(gameId);
+			Player winner = gameService.winner(game);
 			game.addMessage(PerudoMessageType.INFO, "La partie est terminée.");
 			int dices = winner.getFaces().size();
 			String playerName = winner.getPlayerName();
@@ -157,10 +169,10 @@ public class PerudoWebService {
 		System.out.print("bet : game = " + gameId + ", player = " + playerId + ", number = " + number + ", face = " + face);
 		String response = null;
 		try {
-			Game game = gameService().getById(gameId);
-			Player player = playerService().getById(playerId);
+			Game game = gameService.getById(gameId);
+			Player player = playerService.getById(playerId);
 			Declaration declaration = declarationService().createDeclaration(face, number);
-			playerService().bet(player, game, declaration);
+			playerService.bet(player, game, declaration);
 			game.addMessage(PerudoMessageType.INFO, player.getPlayerName() + " a enchéri : " + number + "x" + face + ".");
 			response = SUCCESS + ":bet:" + player.getPlayerName() + ":" + number + ":" + face;
 		}
@@ -181,16 +193,16 @@ public class PerudoWebService {
 		System.out.print("dudo : game = " + gameId + ", player = " + playerId);
 		String response = null;
 		try {
-			Game game = gameService().getById(gameId);
-			Player player = playerService().getById(playerId);
-			playerService().dudo(player, game);
-			if(gameService().isOver(game)){
+			Game game = gameService.getById(gameId);
+			Player player = playerService.getById(playerId);
+			playerService.dudo(player, game);
+			if(gameService.isOver(game)){
 				game.addMessage(PerudoMessageType.INFO, player.getPlayerName() + " a gagné la partie.");
-				response = SUCCESS + ":over:" + gameService().winner(game).getPlayerName();
+				response = SUCCESS + ":over:" + gameService.winner(game).getPlayerName();
 			}
 			else{
 				game.addMessage(PerudoMessageType.INFO, player.getPlayerName() + " a dit Dudo.");
-				response = SUCCESS + ":dudo:" + gameService().activePlayer(game).getPlayerName();
+				response = SUCCESS + ":dudo:" + gameService.activePlayer(game).getPlayerName();
 			}
 		}
 		catch (PerudoException e) {
@@ -210,12 +222,12 @@ public class PerudoWebService {
 		System.out.print("calza : game = " + gameId + ", player = " + playerId);
 		String response = null;
 		try {
-			Game game = gameService().getById(gameId);
-			Player player = playerService().getById(playerId);
-			playerService().calza(player, game);
-			if(gameService().isOver(game)){
+			Game game = gameService.getById(gameId);
+			Player player = playerService.getById(playerId);
+			playerService.calza(player, game);
+			if(gameService.isOver(game)){
 				game.addMessage(PerudoMessageType.INFO, player.getPlayerName() + " a gagné la partie.");
-				response = SUCCESS + ":over:" + gameService().winner(game).getPlayerName();
+				response = SUCCESS + ":over:" + gameService.winner(game).getPlayerName();
 			}
 			else{
 				game.addMessage(PerudoMessageType.INFO, player.getPlayerName() + " a dit Calza.");
