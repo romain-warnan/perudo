437ca862de4b0b335cb83a09496dd89ef9f4c269
Romain Warnan <romain.warnan@gmail.com>
2018-07-24 17:11:58 +0200

    Imports variés

--- src/main/java/fr/plaisance/perudo/domaine/Face.java
+++ src/main/java/fr/plaisance/perudo/domaine/Face.java
@@ -1,10 +1,12 @@
 package fr.plaisance.perudo.domaine;
 
+import fr.plaisance.perudo.exception.IllegalFaceValueException;
+
 import java.util.Objects;
 
 public enum Face {
 
-	PACO		(1),
+	PACO	(1),
 	TWO		(2),
 	THREE	(3),
 	FOUR	(4),
@@ -29,4 +31,22 @@ public enum Face {
 	public void setValue(Integer number) {
 		this.value = number;
 	}
+
+	public static Face of(Integer integer) {
+	    switch (integer) {
+            case 1:
+                return Face.PACO;
+            case 2:
+                return Face.TWO;
+            case 3:
+                return Face.THREE;
+            case 4:
+                return Face.FOUR;
+            case 5:
+                return Face.FIVE;
+            case 6:
+                return Face.SIX;
+        }
+        throw new IllegalFaceValueException();
+    }
 }
--- src/main/java/fr/plaisance/perudo/domaine/Player.java
+++ src/main/java/fr/plaisance/perudo/domaine/Player.java
@@ -1,6 +1,6 @@
 package fr.plaisance.perudo.domaine;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
