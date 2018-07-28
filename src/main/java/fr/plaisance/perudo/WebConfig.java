8035ab15ef3b067687fc5d34d73aa443bf204592
Romain Warnan <romain.warnan@gmail.com>
2018-07-28 22:59:10 +0200

    Swagger

--- src/main/java/fr/plaisance/perudo/WebConfig.java
+++ src/main/java/fr/plaisance/perudo/WebConfig.java
@@ -1,5 +1,6 @@
 package fr.plaisance.perudo;
 
+import fr.plaisance.perudo.rest.FaceConverter;
 import fr.plaisance.perudo.rest.GameConverter;
 import fr.plaisance.perudo.rest.PlayerConverter;
 import fr.plaisance.perudo.service.PlayerService;
@@ -7,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -17,9 +20,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private PlayerConverter playerConverter;
 
+    @Autowired
+    private FaceConverter faceConverter;
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(gameConverter);
         registry.addConverter(playerConverter);
+        registry.addConverter(faceConverter);
     }
 }
