dd5bde74dbd19ca57c660d5de69e37f18437ddf4
Romain Warnan <romain.warnan@gmail.com>
2018-07-29 23:40:39 +0200

    Gestion des exceptions : mauvaise

--- src/main/java/fr/plaisance/perudo/rest/ExceptionController.java
+++ src/main/java/fr/plaisance/perudo/rest/ExceptionController.java
@@ -3,13 +3,14 @@ package fr.plaisance.perudo.rest;
 import fr.plaisance.perudo.exception.PerudoException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionController {
+@RestController
+public class ExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(PerudoException.class)
-    @ResponseBody
     public String exception(PerudoException e) {
         return e.getMessage();
     }
