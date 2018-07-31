b4692f63dd6d3953b1f4912715c5244d3b1d90c6
Romain Warnan <romain.warnan@gmail.com>
2018-07-31 11:39:19 +0200

    Log des requêtes

--- src/main/java/fr/plaisance/perudo/WebConfig.java
+++ src/main/java/fr/plaisance/perudo/WebConfig.java
@@ -1,11 +1,13 @@
 package fr.plaisance.perudo;
 
+import fr.plaisance.perudo.rest.LogInterceptor;
 import fr.plaisance.perudo.rest.converter.FaceConverter;
 import fr.plaisance.perudo.rest.converter.GameConverter;
 import fr.plaisance.perudo.rest.converter.PlayerConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -22,10 +24,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private FaceConverter faceConverter;
 
+    @Autowired
+    private LogInterceptor logInterceptor;
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(gameConverter);
         registry.addConverter(playerConverter);
         registry.addConverter(faceConverter);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor).addPathPatterns("/**");
     }
 }
