41ec6684804e3b549c195fa57543215943cf1a13
Romain Warnan <romain.warnan@insee.fr>
2018-08-07 16:12:07 +0200

    Type number pour le paramètre value

--- frontend/src/Draw.story.js
+++ frontend/src/Draw.story.js
@@ -3,12 +3,12 @@ import { storiesOf } from '@storybook/react'
 import Draw from './Draw.jsx'
 
 storiesOf('Draw/Colors and values', module)
-  .add('Purple five', () => <Draw faces={[1, 2, 3, 4, 5]} color='PURPLE' value='1' />)
-  .add('Red five', () => <Draw faces={[2, 3, 4, 5, 6]} color='BLUE' value='1' />)
-  .add('Yellow four', () => <Draw faces={[1, 2, 3, 4]} color='YELLOW' value='1' />)
-  .add('Green three', () => <Draw faces={[1, 2, 3]} color='GREEN' value='1' />)
-  .add('Red two', () => <Draw faces={[1, 2]} color='RED' value='1' />)
-  .add('Orange one', () => <Draw faces={[1]} color='ORANGE' value='1' />)
+  .add('Purple five', () => <Draw faces={[1, 2, 3, 4, 5]} color='PURPLE' value={1} />)
+  .add('Red five', () => <Draw faces={[2, 3, 4, 5, 6]} color='BLUE' value={1} />)
+  .add('Yellow four', () => <Draw faces={[1, 2, 3, 4]} color='YELLOW' value={1} />)
+  .add('Green three', () => <Draw faces={[1, 2, 3]} color='GREEN' value={1} />)
+  .add('Red two', () => <Draw faces={[1, 2]} color='RED' value={1} />)
+  .add('Orange one', () => <Draw faces={[1]} color='ORANGE' value={1} />)
 
 storiesOf('Draw/Requested value/Regular', module)
   .add('Six', () => <Draw faces={[6, 1, 2, 4, 6]} color='RED' requestedValue={6} />)
